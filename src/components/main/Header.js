41d1d8cb4298f477ed39eebbc253926e0d970c0d
Hyunjin2525 <tktmzpt7@naver.com>
2023-09-13 22:47:38 +0900

    최종완료

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -54,20 +54,6 @@ function Header(){
                                     </li>
                                     </ul>
                             </div>
-
-                            <div className="wrap-icon-header flex-w flex-r-m">
-                                <div className="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
-                                    <i className="zmdi zmdi-search"></i>
-                                </div>
-
-                                <div className="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="2">
-                                    <i className="zmdi zmdi-shopping-cart"></i>
-                                </div>
-
-                                <a href="#" className="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti" data-notify="0">
-                                    <i className="zmdi zmdi-favorite-outline"></i>
-                                </a>
-                            </div>
                         </nav>
                     </div>
                 </div>
